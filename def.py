a28ffe901c19473f2c7f2ecf9f804c6d47f143db
shivam kumar <shivamkumar912278876@gmail.com>
2025-12-02 16:12:25 +0530

    Create def.py

---
+++ def.py
@@ -0,0 +1,7 @@
++5
+Lines changed: 5 additions & 0 deletions
+Original file line number	Diff line number	Diff line change
+@@ -0,0 +1,5 @@
+student(name="Shivam",contact = 123)
+def myfunc(a,b):
+    return a+b, a-b
